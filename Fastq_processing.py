d9d7a6ed8bd12cd912b7455a5e0e98b0603fddb8
Marta <113301961+Inuukin@users.noreply.github.com>
2022-09-12 11:07:58 +0200

    Update Fastq_processing.py

--- Fastq_processing.py
+++ Fastq_processing.py
@@ -12,29 +12,30 @@ import numpy as np
 
 def Fastq_proc():
     
-    ## generate pandas dataframes
+    ## create dataframes
     
     DF = pd.DataFrame()
     DF_stat = pd.DataFrame()
     count = 1
     
-    ## read in fasta file 
-    ## change name of imported fasta file to a name of a file you want to analyse
+    ## read the fasta file in
     
-    for record in SeqIO.parse("reads.fastq", "fastq"): 
+    for record in SeqIO.parse("test.fastq", "fastq"):
             x = record.letter_annotations["phred_quality"]
             df = pd.DataFrame (x, columns = [count])
             DF[count] = list(x)
             count += 1
-
-    ## create a dataframe as required
-    DF_stat['read_position'] = np.arange(len(DF)) 
+            
+            ## create a dataframe as required
+    
+    DF_stat['read_position'] = np.arange(1, len(DF) + 1) 
     DF_stat['mean_Phred_qual'] = DF.mean(axis=1) 
     DF_stat['standard_deviation_Phred_qual'] = DF.std(axis =1)
+    print(DF_stat)
     
     ## Graph
 
-    Graph = DF_stat.plot(kind = "barh", y = "mean_Phred_qual", legend = False, title = "Average read quality",
+    Graph = DF_stat.plot(kind = "barh", x = 'read_position', y = "mean_Phred_qual", legend = False, title = "Average read quality",
         xerr = "standard_deviation_Phred_qual", figsize=(10,20))
 
     Graph.set(ylabel="Base position", xlabel="Phred")
